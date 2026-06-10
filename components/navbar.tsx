9abfaeb60f0181af916bcc93d1be94549e53565e
Aargh Rai <shubh2007rai@gmail.com>
2026-06-10 22:52:08 +0530

    hero & navbar?

---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+export function Navbar() {
+  return (
+    <div className="flex w-full justify-between pl-6 pr-6 pt-2">
+    <div>LOGO</div>
+    <div className="flex gap-4">
+      <a href="#timeline">Timeline</a>
+      <a href="#registration">Registration</a>
+      <a href="#problem_statements">Problem Statements</a>
+      <a href="#organiser">Organiser</a>
+      <a href="#partner">Partner</a>
+      <a href="#content">Content</a>
+    </div>
+    </div>
+  )
+}
